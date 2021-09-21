9e35f02a9cb4fb6f0162b127553e1e1f4bb86ca6
Wes160 <masonmeyer160@gmail.com>
2021-09-21 12:30:33 -0500

    Initial Commit

---
+++ Naruto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Naruto
+//
+//  Created by PMStudent on 9/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
